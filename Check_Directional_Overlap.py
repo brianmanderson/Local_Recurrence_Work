43d179637ac37af372e3bee008fde991e77cf8f4
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2019-12-05 12:50:51 -0600

    first push

---
+++ Check_Directional_Overlap.py
@@ -0,0 +1,9 @@
+__author__ = 'Brian M Anderson'
+# Created on 12/5/2019
+
+'''
+This should have two parts... first, check the recurrence image for what direction
+the recurrence occurred
+Then, look at the post-treatment image and see if there was 5 mm margin existing in that direction
+'''
+
