96306e15259d7f83d1c4ae821828df92dc06e141
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2019-12-06 09:53:54 -0600

    working towards a spherical overlay, checking needed angles

--- Check_Directional_Overlap.py
+++ Check_Directional_Overlap.py
@@ -8,6 +8,45 @@ import SimpleITK as sitk
 import time
 
 
+def cartesian_to_polar(xyz):
+    '''
+    :param x: x_values in single array
+    :param y: y_values in a single array
+    :param z: z_values in a single array
+    :return: polar coordinates in the form of: radius, rotation away from the z axis, and rotation from the y axis
+    '''
+    # xyz = np.stack([x, y, z], axis=-1)
+    input_shape = xyz.shape
+    reshape = False
+    if len(input_shape) > 2:
+        reshape = True
+        xyz = np.reshape(xyz,[np.prod(xyz.shape[:-1]),3])
+    polar_points = np.empty(xyz.shape)
+    # ptsnew = np.hstack((xyz, np.empty(xyz.shape)))
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    polar_points[:,0] = np.sqrt(xy + xyz[:,2]**2)
+    polar_points[:,1] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    polar_points[:,2] = np.arctan2(xyz[:,1], xyz[:,0])
+    if reshape:
+        polar_points = np.reshape(polar_points,input_shape)
+    return polar_points
+
+
+def polar_to_cartesian(polar_xyz):
+    '''
+    :param polar_xyz: in the form of radius, elevation away from z axis, and elevation from y axis
+    :return: x, y, and z intensities
+    '''
+    cartesian_points = np.empty(polar_xyz.shape)
+    from_y = polar_xyz[:,2]
+    xy_plane = np.sin(polar_xyz[:,1])*polar_xyz[:,0]
+    cartesian_points[:,2] = np.cos(polar_xyz[:,1])*polar_xyz[:,0]
+    cartesian_points[:,0] = np.sin(from_y)*xy_plane
+    cartesian_points[:,1] = np.cos(from_y)*xy_plane
+    return cartesian_points
+
+
 def create_ray_spectrum(image,origin, radius=50,spacing=(0.975,0.975,5)):
     '''
     :param image:
@@ -45,12 +84,20 @@ def create_ray_spectrum(image,origin, radius=50,spacing=(0.975,0.975,5)):
     return temp_image
 
 
+def create_distance_field(image,origin, spacing=(0.975,0.975,5.0)):
+    array_of_points = np.where(image==1)
+    z = array_of_points[0]
+    x = array_of_points[1]
+    y = array_of_points[2]
+    xxx = 1
+    return None
+
 '''
 This should have two parts... first, check the recurrence image for what direction
 the recurrence occurred
 Then, look at the post-treatment image and see if there was 5 mm margin existing in that direction
 '''
-# new_images = np.load(os.path.join('..', 'saved.npy'))
+new_images = np.load(os.path.join('..', 'saved.npy'))
 images_path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Recurrence_Data\Images'
 excel_file = os.path.join('..','Data','Post_treatment_and_Recurrence_info.xlsx')
 data = pd.read_excel(excel_file)
@@ -70,6 +117,7 @@ for index in range(len(MRNs)):
     centroid_of_ablation = center_of_mass(mask[...,3])
     recurrence = recurrence_reader.mask[...,2]
     spacing = recurrence_reader.annotation_handle.GetSpacing()
+    create_distance_field(recurrence,origin=centroid_of_ablation, spacing=spacing)
     start = time.time()
     new_mask = create_ray_spectrum(recurrence,centroid_of_ablation,100, spacing=spacing)
     print(time.time()-start)
