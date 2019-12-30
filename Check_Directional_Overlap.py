2d206aff31722a75884ea9416f3f630a766c2b4d
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2019-12-30 13:12:08 -0600

    Pull contours to be within the liver on whichever scan they were originally defined

--- Check_Directional_Overlap.py
+++ Check_Directional_Overlap.py
@@ -28,15 +28,16 @@ for index in range(len(MRNs)):
     if os.path.exists(os.path.join(status_path,MRN+'.txt')):
         continue
     recurrence_path = os.path.join(images_path,MRN,Recurrence)
-    recurrence_reader = Dicom_to_Imagestack(arg_max=False,Contour_Names=['Liver','recurrence','Ablation_Recurrence',
-                                                                         'Ablation','GTV_Exp_5mm_outside_Ablation'])
+    recurrence_reader = Dicom_to_Imagestack(arg_max=False,Contour_Names=['Liver_recurrence','recurrence','Ablation_Recurrence',
+                                                                         'Liver_Ablation','Ablation','GTV_Exp_5mm_outside_Ablation'])
     recurrence_reader.Make_Contour_From_directory(recurrence_path)
 
     mask = recurrence_reader.mask
-    liver = mask[...,1]
-    mask[liver==0] = 0
+    liver_recurrence = mask[...,1]
     ablation_recurrence = mask[...,3]
     recurrence = mask[...,2]
+    ablation_recurrence[liver_recurrence==0] = 0
+    recurrence[liver_recurrence==0] = 0
     centroid_of_ablation_recurrence = np.asarray(center_of_mass(ablation_recurrence))
     spacing = recurrence_reader.annotation_handle.GetSpacing()
     polar_cords = create_distance_field(recurrence,origin=centroid_of_ablation_recurrence, spacing=spacing)
@@ -51,24 +52,27 @@ for index in range(len(MRNs)):
     
     Note: This will turn a star shape into a square which encompasses the star!
     '''
-    cone_cords = create_distance_field(np.ones(liver.shape),origin=centroid_of_ablation_recurrence,spacing=spacing)
+    cone_cords = create_distance_field(np.ones(liver_recurrence.shape),origin=centroid_of_ablation_recurrence,spacing=spacing)
     cone_cords = np.round(cone_cords,4)
     output = np.zeros(cone_cords.shape[0])
     vals = np.where((cone_cords[:,1]>=min_phi)&(cone_cords[:,1]<=max_phi)&(cone_cords[:,2]>=min_theta)&(cone_cords[:,2]<=max_theta))
     output[vals[0]] = 1
-    output_recurrence = np.reshape(output,liver.shape) # This is now a cone including the recurrence site
+    output_recurrence = np.reshape(output,liver_recurrence.shape) # This is now a cone including the recurrence site
     output_recurrence = np.expand_dims(output_recurrence, axis=-1)
     output_recurrence = np.repeat(output_recurrence,repeats=3,axis=-1)
 
-    ablation = mask[...,4]
-    min_ablation_margin = mask[...,5]
+    liver_ablation = mask[...,4]
+    ablation = mask[...,5]
+    min_ablation_margin = mask[...,6]
+    ablation[liver_ablation==0] == 0
+    min_ablation_margin[liver_ablation==0] = 0
     centroid_of_ablation = np.asarray(center_of_mass(ablation))
-    cone_cords = create_distance_field(np.ones(liver.shape),origin=centroid_of_ablation,spacing=spacing)
+    cone_cords = create_distance_field(np.ones(liver_ablation.shape),origin=centroid_of_ablation,spacing=spacing)
     cone_cords = np.round(cone_cords,4)
     output = np.zeros(cone_cords.shape[0])
     vals = np.where((cone_cords[:,1]>=min_phi)&(cone_cords[:,1]<=max_phi)&(cone_cords[:,2]>=min_theta)&(cone_cords[:,2]<=max_theta))
     output[vals[0]] = 1
-    output = np.reshape(output,liver.shape) # This is now a cone including the recurrence site
+    output = np.reshape(output,liver_ablation.shape) # This is now a cone including the recurrence site
     overlap = np.where((output==1) & (min_ablation_margin==1)) # See if it overlaps with the minimum ablation margin
     if overlap:
         data['Overlap?'][index] = 1.0
