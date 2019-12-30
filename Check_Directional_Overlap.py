772ce0b97682ca2124279f33c2eb94e020afc553
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2019-12-30 10:03:33 -0600

    Ensuring that polar coordinates to cartesian is reversible, seems theta comes from negative y axis

--- Check_Directional_Overlap.py
+++ Check_Directional_Overlap.py
@@ -10,15 +10,16 @@ import time
 
 def cartesian_to_polar(zxy):
     '''
-    :param xyz: array of xyz cooridnates
-    :return: polar coordinates in the form of: radius, rotation away from the z axis (phi), and rotation from the y axis (theta)
+    :param xyz: array of zxy cooridnates
+    :return: polar coordinates in the form of: radius, rotation away from the z axis (phi), and rotation from the
+    negative y axis (theta)
     '''
     # xyz = np.stack([x, y, z], axis=-1)
     input_shape = zxy.shape
     reshape = False
     if len(input_shape) > 2:
         reshape = True
-        xyz = np.reshape(zxy,[np.prod(zxy.shape[:-1]),3])
+        zxy = np.reshape(zxy,[np.prod(zxy.shape[:-1]),3])
     polar_points = np.empty(zxy.shape)
     # ptsnew = np.hstack((xyz, np.empty(xyz.shape)))
     xy = zxy[:,2]**2 + zxy[:,1]**2
@@ -26,7 +27,7 @@ def cartesian_to_polar(zxy):
     polar_points[:,1] = np.arctan2(np.sqrt(xy), zxy[:,0])  # for elevation angle defined from Z-axis down
     #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
     polar_points[:,2] = np.arctan2(zxy[:,1], zxy[:,2])
-    # polar_points[:, 2][polar_points[:, 2] < 0] += 2 * np.pi  # Make them all 0 to 2 pi for theta
+    polar_points[:, 2][polar_points[:, 2] < 0] += 2 * np.pi  # Make them all 0 to 2 pi for theta
     if reshape:
         polar_points = np.reshape(polar_points,input_shape)
     return polar_points
@@ -34,7 +35,7 @@ def cartesian_to_polar(zxy):
 
 def polar_to_cartesian(polar):
     '''
-    :param polar: in the form of radius, elevation away from z axis, and elevation from x axis
+    :param polar: in the form of radius, angle away from z axis, and angle from the negative y axis
     :return: z, x, y intensities as differences from the origin
     '''
     cartesian_points = np.empty(polar.shape)
@@ -87,10 +88,21 @@ def create_ray_spectrum(image,origin, radius=50,spacing=(0.975,0.975,5),theta_ra
 
 
 def create_distance_field(image,origin, spacing=(0.975,0.975,5.0)):
+    '''
+    :param image:
+    :param origin:
+    :param spacing:
+    :return: polar coordinates, in: [radius, rotation away from the z axis (phi), and rotation from the y axis (theta)]
+    '''
     array_of_points = np.transpose(np.asarray(np.where(image==1)),axes=(1,0))
     spacing_aranged = np.asarray([spacing[2],spacing[0],spacing[1]])
     differences = (array_of_points - origin)*spacing_aranged
     polar_coordinates = cartesian_to_polar(differences)
+    image_2 = np.zeros(image.shape)
+    k = polar_to_cartesian(polar_coordinates)
+    k += origin
+    k = round(k)
+    image_2[k] = 1
     return polar_coordinates
 
 
@@ -105,21 +117,34 @@ excel_file = os.path.join('..','Data','Post_treatment_and_Recurrence_info.xlsx')
 data = pd.read_excel(excel_file)
 MRNs = data['MRN']
 for index in range(len(MRNs)):
+    index = 1
     MRN = str(data['MRN'][index])
     Secondary = data['Secondary'][index]
     Recurrence = data['Recurrence'][index]
     recurrence_path = os.path.join(images_path,MRN,Recurrence)
-    recurrence_reader = Dicom_to_Imagestack(arg_max=False,Contour_Names=['Liver','recurrence','Ablation'])
+    recurrence_reader = Dicom_to_Imagestack(arg_max=False,Contour_Names=['Liver','recurrence','Ablation_Recurrence',
+                                                                         'Ablation','GTV_Exp_5mm_outside_Ablation'])
     recurrence_reader.Make_Contour_From_directory(recurrence_path)
 
     mask = recurrence_reader.mask
     liver = mask[...,1]
-    mask[liver == 0] = 0  # Bring everything to be within the liver
     # centroid_of_recurrence = center_of_mass(mask[...,2])
     centroid_of_ablation = np.asarray(center_of_mass(mask[...,3]))
     recurrence = recurrence_reader.mask[...,2]
     spacing = recurrence_reader.annotation_handle.GetSpacing()
+    recurrence = np.zeros([10,200,200])
+    recurrence[5,:50,:50] = 1
+    centroid_of_ablation = (5,100,100)
+    liver = np.zeros(recurrence.shape)
+    spacing = (1,1,1)
     polar_cords = create_distance_field(recurrence,origin=centroid_of_ablation, spacing=spacing)
+    min_phi, max_phi, min_theta, max_theta = min(polar_cords[...,1]), max(polar_cords[...,1]), min(polar_cords[...,2]),\
+                                             max(polar_cords[...,2])
+    cone_cords = create_distance_field(np.ones(liver.shape),origin=centroid_of_ablation,spacing=spacing)
+    output = np.zeros(cone_cords.shape[0])
+    vals = np.where((cone_cords[:,1]>min_phi)&(cone_cords[:,1]<max_phi)&(cone_cords[:,2]>min_theta)&(cone_cords[:,2]<max_theta))
+    output[vals[0]] = 1
+    output = np.reshape(output,liver.shape)
     max_radius = np.max(polar_cords[:,0])
     start = time.time()
     zz, xx, yy = np.mgrid[0:recurrence.shape[0],0:recurrence.shape[1],0:recurrence.shape[2]]
@@ -127,9 +152,6 @@ for index in range(len(MRNs)):
              ((yy - centroid_of_ablation[2])*spacing[2]) ** 2)
     out = np.zeros(recurrence.shape)
     out[circle<max_radius] = 1
-    new_mask = create_ray_spectrum(recurrence,origin=centroid_of_ablation,radius=int(np.max(polar_coods)+1), spacing=spacing,
-                                   theta_range=np.unique(np.round(np.rad2deg(polar_coods[:,2]))),
-                                   phi_range=np.unique(np.round(np.rad2deg(polar_coods[:,1]))))
     print(time.time()-start)
     break
 xxx = 1
