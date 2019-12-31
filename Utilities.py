b97c644cb8b00a985eca35d95d6b740ffd35be08
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2019-12-31 09:37:31 -0600

    pushing back theta to be 0 to pi and -0 to -pi
    Breaking up cone creation into the two halves

--- Utilities.py
+++ Utilities.py
@@ -22,7 +22,7 @@ def cartesian_to_polar(zxy):
     polar_points[:,1] = np.arctan2(np.sqrt(xy), zxy[:,0])  # for elevation angle defined from Z-axis down
     #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
     polar_points[:,2] = np.arctan2(zxy[:,2], zxy[:,1])
-    polar_points[:, 2][polar_points[:, 2] < 0] += 2 * np.pi  # Make them all 0 to 2 pi for theta
+    # polar_points[:, 2][polar_points[:, 2] < 0] += 2 * np.pi  # let them range +0 to +pi and -0 to -pi
     if reshape:
         polar_points = np.reshape(polar_points,input_shape)
     return polar_points
@@ -56,42 +56,53 @@ def create_distance_field(image,origin, spacing=(0.975,0.975,5.0)):
     return polar_coordinates
 
 
-def define_cone(polar_cords, centroid_of_ablation_recurrence,liver_recurrence, spacing, margin=100, min_max=False,
+def define_cone(polar_cords_base, centroid_of_ablation_recurrence,liver_recurrence, spacing, margin=100, min_max=False,
                 margin_degree=np.deg2rad(1.5)):
     '''
-    :param polar_cords: polar coordinates from ablation_recurrence centroid to recurrence
+    :param polar_cords_base: polar coordinates from ablation_recurrence centroid to recurrence, come in [phi, theta]
+    where theta ranges from 0 to pi and -0 to -pi
     :param centroid_of_ablation_recurrence: centroid of ablation recurrence
     :param liver_recurrence: shape used to make output
     :param margin: how far would you like to look, in mm
     :param margin_degree: degrees of wiggle allowed, recommend at least 0.5 degrees
     :return:
     '''
-    polar_cords = polar_cords.astype('float16')
-    if polar_cords.shape[1] == 3:
-        polar_cords = polar_cords[:,1:]
-    cone_cords = create_distance_field(np.ones(liver_recurrence.shape),origin=centroid_of_ablation_recurrence,spacing=spacing)
-    cone_cords = np.round(cone_cords,3).astype('float16')
-    output = np.zeros(cone_cords.shape[0])
-    min_phi, max_phi, min_theta, max_theta = min(polar_cords[..., 0]), max(polar_cords[..., 0]), min(
-        polar_cords[..., 1]), max(polar_cords[..., 1])
-    if min_max:
-        vals = np.where((cone_cords[:, 1] >= min_phi) & (cone_cords[:, 1] <= max_phi) & (cone_cords[:, 2] >= min_theta)
-                        & (cone_cords[:, 2] <= max_theta))
-        del cone_cords
-        output[vals[0]] = 1
-    else:
-        vals = np.where(cone_cords[:, 0] < margin)
-        cone_cords_reduced = cone_cords[vals[0]][:,1:]
-        del cone_cords
-        difference = cone_cords_reduced[:,None] - polar_cords
-        min_dif = np.min(difference**2,axis=1) # find the minimum difference for each point against the ablation region
-        del difference
-        total_dif = np.sqrt(np.sum(min_dif,axis=1))
-        del min_dif
-        dif_vals = np.where(total_dif<margin_degree) # Allow 2 degrees of wiggle
-        output[vals[0][dif_vals[0]]] = 1
-        del dif_vals
-    del vals
+    polar_cords_base = polar_cords_base.astype('float16')
+    if polar_cords_base.shape[1] == 3:
+        polar_cords_base = polar_cords_base[:,1:]
+    cone_cords_base = create_distance_field(np.ones(liver_recurrence.shape),origin=centroid_of_ablation_recurrence,spacing=spacing)
+    cone_cords_base = np.round(cone_cords_base,3).astype('float16')
+    output = np.zeros(cone_cords_base.shape[0])
+    positive_polar_indexes = polar_cords_base[:,1] > 0
+    negative_polar_indexes = ~positive_polar_indexes
+    positive_cord_indexes = cone_cords_base[:,2] > 0
+    negative_cord_indexes = ~positive_cord_indexes
+    for polar_indxes, cord_indexes in zip([positive_polar_indexes, negative_polar_indexes],
+                                          [positive_cord_indexes,negative_cord_indexes]):
+        polar_cords = polar_cords_base[polar_indxes]
+        cone_cords = cone_cords_base[cord_indexes]
+        if not polar_cords or not cone_cords:
+            continue
+        min_phi, max_phi, min_theta, max_theta = min(polar_cords[..., 0]), max(polar_cords[..., 0]), min(
+            polar_cords[..., 1]), max(polar_cords[..., 1])
+        if min_max:
+            vals = np.where((cone_cords[:, 1] >= min_phi) & (cone_cords[:, 1] <= max_phi) & (cone_cords[:, 2] >= min_theta)
+                            & (cone_cords[:, 2] <= max_theta))
+            del cone_cords
+            output[cord_indexes][vals[0]] = 1
+        else:
+            vals = np.where(cone_cords[:, 0] < margin)
+            cone_cords_reduced = cone_cords[vals[0]][:,1:]
+            del cone_cords
+            difference = cone_cords_reduced[:,None] - polar_cords
+            min_dif = np.min(difference**2,axis=1) # find the minimum difference for each point against the ablation region
+            del difference
+            total_dif = np.sqrt(np.sum(min_dif,axis=1))
+            del min_dif
+            dif_vals = np.where(total_dif<margin_degree) # Allow 2 degrees of wiggle
+            output[cord_indexes][vals[0][dif_vals[0]]] = 1
+            del dif_vals
+        del vals
     output = np.reshape(output,liver_recurrence.shape) # This is now a cone including the recurrence site
     return output
 
