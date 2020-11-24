3ecbc96179595682f6c4550e0d7bcf53b5987904
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-24 12:03:45 -0600

    can now pull entire training dataset by setting id=-1

--- Outcome_Analysis/DeepLearningTools/ReturnGenerators.py
+++ Outcome_Analysis/DeepLearningTools/ReturnGenerators.py
@@ -5,8 +5,9 @@ from Base_Deeplearning_Code.Data_Generators.Image_Processors_Module.Image_Proces
 from Local_Recurrence_Work.Outcome_Analysis.DeepLearningTools.ReturnPaths import return_paths, os
 
 
-def return_generators(batch_size=5, wanted_keys={'inputs': ('image',), 'outputs': ('annotation',)},
-                      cross_validation_id=0, cache=True, evaluate=False):
+def return_generators(batch_size=5, wanted_keys={'inputs': ('combined',), 'outputs': ('annotation',)},
+                      cross_validation_id=0, cache=False, evaluate=False,
+                      build_keys=('primary_image','secondary_image_deformed', 'primary_liver')):
     """
     :param batch_size:
     :param wanted_keys:
@@ -14,21 +15,33 @@ def return_generators(batch_size=5, wanted_keys={'inputs': ('image',), 'outputs'
     :param cache:
     :return: base_path, morfeus_drive, train_generator, validation_generator
     """
+    '''
+    The keys within the dictionary are: 'primary_image', 'secondary_image', 'secondary_image_deformed'
+    '''
     base_path, morfeus_drive = return_paths()
-    train_recurrence_path = [os.path.join(base_path, 'CV_{}'.format(cross_validation_id),
-                                          'Train_Recurrence')]
-    validation_recurrence_path = [os.path.join(base_path, 'CV_{}'.format(cross_validation_id),
-                                               'Validation_Recurrence')]
+    if cross_validation_id != -1:
+        train_recurrence_path = [os.path.join(base_path, 'CrossValidation', 'CV_{}'.format(cross_validation_id),
+                                              'Train_Recurrence')]
+        validation_recurrence_path = [os.path.join(base_path, 'CrossValidation', 'CV_{}'.format(cross_validation_id),
+                                                   'Validation_Recurrence')]
 
-    train_no_recurrence_path = [os.path.join(base_path, 'CV_{}'.format(cross_validation_id),
-                                             'Train_No_Recurrence')]
-    validation_no_recurrence_path = [os.path.join(base_path, 'CV_{}'.format(cross_validation_id),
-                                                  'Validation_No_Recurrence')]
+        train_no_recurrence_path = [os.path.join(base_path, 'CrossValidation', 'CV_{}'.format(cross_validation_id),
+                                                 'Train_No_Recurrence')]
+        validation_no_recurrence_path = [os.path.join(base_path, 'CrossValidation', 'CV_{}'.format(cross_validation_id),
+                                                      'Validation_No_Recurrence')]
+    else:
+        train_recurrence_path = [os.path.join(base_path, 'Recurrence')]
+        validation_recurrence_path = None
+
+        train_no_recurrence_path = [os.path.join(base_path, 'No_Recurrence')]
+        validation_no_recurrence_path = None
     train_recurrence_generator = DataGeneratorClass(record_paths=train_recurrence_path)
     train_no_recurence_generator = DataGeneratorClass(record_paths=train_no_recurrence_path)
 
-    validation_recurrence_generator = DataGeneratorClass(record_paths=validation_recurrence_path)
-    validation_no_recurrence_generator = DataGeneratorClass(record_paths=validation_no_recurrence_path)
+    validation_recurrence_generator, validation_no_recurrence_generator = None, None
+    if validation_recurrence_path is not None:
+        validation_recurrence_generator = DataGeneratorClass(record_paths=validation_recurrence_path)
+        validation_no_recurrence_generator = DataGeneratorClass(record_paths=validation_no_recurrence_path)
 
     for train_generator, validation_generator, train_path, val_path in zip([train_recurrence_generator,
                                                                             train_no_recurence_generator],
@@ -38,29 +51,40 @@ def return_generators(batch_size=5, wanted_keys={'inputs': ('image',), 'outputs'
                                                                             train_no_recurrence_path],
                                                                            [validation_recurrence_path,
                                                                             validation_no_recurrence_path]):
-        train_processors, validation_processors = [], []
+
+        train_processors = [
+            ExpandDimension(axis=-1, image_keys=build_keys),
+            Cast_Data(key_type_dict={'primary_liver': 'float32'})
+        ]
+        validation_processors = [
+            ExpandDimension(axis=-1, image_keys=build_keys),
+            Cast_Data(key_type_dict={'primary_liver': 'float32'})
+        ]
         if cache:
             train_processors += [
                 {'cache': os.path.join(train_path[0], 'cache')}
             ]
-            validation_processors += [
-                {'cache': os.path.join(val_path[0], 'cache')}
-            ]
+            if validation_recurrence_path is not None:
+                validation_processors += [
+                    {'cache': os.path.join(val_path[0], 'cache')}
+                ]
         train_processors += [
-            Flip_Images(keys=('image',), flip_lr=True, flip_up_down=True, flip_3D_together=True,flip_z=True)
-        ]
-        train_processors += [
+            CombineKeys(image_keys=build_keys, output_key='combined'),
+            Flip_Images(keys=('combined',), flip_lr=True, flip_up_down=True, flip_3D_together=True, flip_z=True),
             Return_Outputs(wanted_keys),
-            {'shuffle': len(train_generator)}]
+            {'shuffle': len(train_generator)}
+        ]
         validation_processors += [
+            CombineKeys(image_keys=build_keys, output_key='combined'),
             Return_Outputs(wanted_keys)]
         if batch_size != 0:
             train_processors += [{'batch': batch_size//2}]
             validation_processors += [{'batch': 1}]
         train_processors += [{'repeat'}]
         validation_processors += [{'repeat'}]
-        train_generator.compile_data_set(image_processors=train_processors, debug=False)
-        validation_generator.compile_data_set(image_processors=validation_processors, debug=False)
+        train_generator.compile_data_set(image_processors=train_processors, debug=True)
+        if validation_no_recurrence_generator is not None:
+            validation_generator.compile_data_set(image_processors=validation_processors, debug=True)
 
     '''
     Now, we want to provide the model with examples of both recurrence and non_recurrence each time
@@ -70,12 +94,14 @@ def return_generators(batch_size=5, wanted_keys={'inputs': ('image',), 'outputs'
     train_generator = train_recurrence_generator
     train_generator.data_set = train_dataset
     train_generator.total_examples += train_no_recurence_generator.total_examples
-    validation_generator = validation_recurrence_generator
-    validation_dataset = tf.data.Dataset.zip((validation_recurrence_generator.data_set,
-                                              validation_no_recurrence_generator.data_set)).map(
-        lambda x, y: ((tf.concat((x[0][0], y[0][0]), axis=0),), (tf.concat((x[1][0], y[1][0]), axis=0),)))
-    validation_generator.data_set = validation_dataset.unbatch().batch(1)
-    validation_generator.total_examples += validation_no_recurrence_generator.total_examples
+    validation_generator = None
+    if validation_recurrence_generator is not None:
+        validation_generator = validation_recurrence_generator
+        validation_dataset = tf.data.Dataset.zip((validation_recurrence_generator.data_set,
+                                                  validation_no_recurrence_generator.data_set)).map(
+            lambda x, y: ((tf.concat((x[0][0], y[0][0]), axis=0),), (tf.concat((x[1][0], y[1][0]), axis=0),)))
+        validation_generator.data_set = validation_dataset.unbatch().batch(1)
+        validation_generator.total_examples += validation_no_recurrence_generator.total_examples
     generators = [validation_generator]
     # generators += [train_generator]
     if evaluate:
