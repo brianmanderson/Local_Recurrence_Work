7d58067727d38de75679f1922010875eaa8c41e1
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-24 16:45:53 -0600

    create __init__ file

--- Outcome_Analysis/DeepLearningTools/ReturnModels.py
+++ Outcome_Analysis/DeepLearningTools/ReturnModels.py
@@ -1,9 +1,12 @@
 __author__ = 'Brian M Anderson'
 # Created on 11/24/2020
 from Local_Recurrence_Work.Outcome_Analysis.DeepLearningTools.DenseNetModel.MyDenseNet import MyDenseNet121
+from tensorflow.keras.mixed_precision import experimental
 
 
 def return_model(model_type='2D_Vanilla'):
+    policy = experimental.Policy('mixed_float16')
+    experimental.set_policy(policy)
     if model_type == '2D_Vanilla':
         return MyDenseNet121(include_top=False, input_shape=(32, 128, 128, 3), classes=2)
     else:
--- Outcome_Analysis/DeepLearningTools/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'Brian M Anderson'
-# Created on 11/24/2020
-
