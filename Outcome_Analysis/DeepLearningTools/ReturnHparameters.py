10249913b3f74bd4221b107344f76d26ee4d78a3
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-30 10:32:24 -0600

    back to 5, 10, 20

--- Outcome_Analysis/DeepLearningTools/ReturnHparameters.py
+++ Outcome_Analysis/DeepLearningTools/ReturnHparameters.py
@@ -1,7 +1,6 @@
 __author__ = 'Brian M Anderson'
 # Created on 11/28/2020
-from Deep_Learning.Base_Deeplearning_Code.Finding_Optimization_Parameters.HyperParameters import return_hparams,\
-    return_pandas_df, OrderedDict
+from Deep_Learning.Base_Deeplearning_Code.Finding_Optimization_Parameters.HyperParameters import OrderedDict
 
 
 def return_list_of_models(model_type=0):
@@ -10,9 +9,9 @@ def return_list_of_models(model_type=0):
         base_dict0 = lambda min_lr, max_lr, step_factor: \
             OrderedDict({'Model_Type': model_type, 'min_lr': min_lr, 'max_lr': max_lr, 'step_factor': step_factor})
         dictionary = [
-            base_dict0(min_lr=1e-4, max_lr=3e-2, step_factor=1),
-            base_dict0(min_lr=1e-4, max_lr=3e-2, step_factor=3),
-            base_dict0(min_lr=1e-4, max_lr=3e-2, step_factor=5)
+            base_dict0(min_lr=1e-4, max_lr=3e-2, step_factor=5),
+            base_dict0(min_lr=1e-4, max_lr=3e-2, step_factor=10),
+            base_dict0(min_lr=1e-4, max_lr=3e-2, step_factor=20)
         ]
         model_dictionary[0] = dictionary
     return model_dictionary
