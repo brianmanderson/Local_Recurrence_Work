85f68983fa29c5cd9f7b88a9a9b8684e79a141ea
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-30 09:01:34 -0600

    add learning rate output

--- Outcome_Analysis/DeepLearningTools/RunModel.py
+++ Outcome_Analysis/DeepLearningTools/RunModel.py
@@ -2,6 +2,7 @@ __author__ = 'Brian M Anderson'
 # Created on 11/24/2020
 import tensorflow as tf
 from Deep_Learning.Base_Deeplearning_Code.Cyclical_Learning_Rate.clr_callback_TF2 import SGDRScheduler
+from Deep_Learning.Base_Deeplearning_Code.Callbacks.TF2_Callbacks import Add_Images_and_LR
 import os
 from tensorboard.plugins.hparams.keras import Callback
 
@@ -13,7 +14,10 @@ def run_model(model, train_generator, validation_generator, min_lr, max_lr, mode
                                                     save_freq='epoch', save_best_only=True, save_weights_only=True)
     tensorboard = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path, profile_batch=0,
                                                  write_graph=True)  # profile_batch='300,401',
-    lrate = SGDRScheduler(min_lr=min_lr, max_lr=max_lr, steps_per_epoch=len(train_generator), lr_decay=0.98)
+
+    lrate = SGDRScheduler(min_lr=min_lr, max_lr=max_lr, steps_per_epoch=len(train_generator), cycle_length=step_factor,
+                          lr_decay=0.98)
+    add_lr = Add_Images_and_LR(log_dir=tensorboard_path, add_images=False)
     callbacks = [tensorboard, lrate]
     if hparams is not None:
         hp_callback = Callback(tensorboard_path, hparams=hparams, trial_id='Trial_ID:{}'.format(trial_id))
@@ -22,7 +26,7 @@ def run_model(model, train_generator, validation_generator, min_lr, max_lr, mode
     print('\n\n\n\nRunning {}\n\n\n\n'.format(tensorboard_path))
     model.compile(optimizer, loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
                   metrics=[tf.keras.metrics.CategoricalAccuracy()])
-    model.fit(train_generator.data_set, epochs=epochs, steps_per_epoch=len(train_generator)*step_factor,
+    model.fit(train_generator.data_set, epochs=epochs, steps_per_epoch=len(train_generator),
               validation_data=validation_generator.data_set, validation_steps=len(validation_generator),
               validation_freq=1, callbacks=callbacks)
     model.save(os.path.join(model_path, 'final_model.h5'))
