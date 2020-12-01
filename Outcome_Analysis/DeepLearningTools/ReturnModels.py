c7bfe1927ccc0f657e41cc724bdf96f87d720811
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-01 08:34:12 -0600

    don't setpolicy more than once

--- Outcome_Analysis/DeepLearningTools/ReturnModels.py
+++ Outcome_Analysis/DeepLearningTools/ReturnModels.py
@@ -1,12 +1,9 @@
 __author__ = 'Brian M Anderson'
 # Created on 11/24/2020
 from Local_Recurrence_Work.Outcome_Analysis.DeepLearningTools.DenseNetModel.MyDenseNet import MyDenseNet121
-from tensorflow.keras.mixed_precision import experimental
 
 
 def return_model(model_key=0):
-    policy = experimental.Policy('mixed_float16')
-    experimental.set_policy(policy)
     if model_key == 0:
         return MyDenseNet121(include_top=False, input_shape=(32, 128, 128, 3), classes=2, include_3d=False)
     elif model_key == 1:
