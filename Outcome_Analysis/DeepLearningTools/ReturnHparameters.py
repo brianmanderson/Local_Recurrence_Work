3ebbbbf1d9667e19ad62780573e93a9f503ef946
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-01 15:44:08 -0600

    run for 5 epochs, check out reduced lrs

--- Outcome_Analysis/DeepLearningTools/ReturnHparameters.py
+++ Outcome_Analysis/DeepLearningTools/ReturnHparameters.py
@@ -17,7 +17,8 @@ def return_list_of_models(model_key=0):
         base_dict0 = lambda min_lr, max_lr, step_factor: \
             OrderedDict({'Model_Type': model_key, 'min_lr': min_lr, 'max_lr': max_lr, 'step_factor': step_factor})
         dictionary = [
-            base_dict0(min_lr=8e-5, max_lr=4e-2, step_factor=10),
+            base_dict0(min_lr=8e-5, max_lr=1e-3, step_factor=10),
+            base_dict0(min_lr=8e-5, max_lr=8e-4, step_factor=10),
         ]
     model_dictionary = {model_key: dictionary}
     return model_dictionary
--- Outcome_Analysis/DeepLearningTools/Run2DModel.py
+++ Outcome_Analysis/DeepLearningTools/Run2DModel.py
@@ -25,7 +25,7 @@ def run_2d_model(batch_size=24, model_key=0):
         _, _, train_generator, validation_generator = return_generators(batch_size=batch_size,
                                                                         cross_validation_id=cv_id,
                                                                         cache=True)
-        for iteration in range(10):
+        for iteration in range(5):
             for model_parameters in list_of_models:
                 base_df = pd.read_excel(excel_path)
                 base_df.set_index('Model_Index')
