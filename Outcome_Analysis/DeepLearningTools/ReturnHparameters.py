b08bc8bc0922c223aa2e1d04e0fa3f058d8157da
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-01 09:59:35 -0600

    rename to be model_key

--- Outcome_Analysis/DeepLearningTools/ReturnHparameters.py
+++ Outcome_Analysis/DeepLearningTools/ReturnHparameters.py
@@ -3,23 +3,23 @@ __author__ = 'Brian M Anderson'
 from Deep_Learning.Base_Deeplearning_Code.Finding_Optimization_Parameters.HyperParameters import OrderedDict
 
 
-def return_list_of_models(model_type=0):
+def return_list_of_models(model_key=0):
     dictionary = []
-    if model_type == 0:
+    if model_key == 0:
         base_dict0 = lambda min_lr, max_lr, step_factor: \
-            OrderedDict({'Model_Type': model_type, 'min_lr': min_lr, 'max_lr': max_lr, 'step_factor': step_factor})
+            OrderedDict({'Model_Type': model_key, 'min_lr': min_lr, 'max_lr': max_lr, 'step_factor': step_factor})
         dictionary = [
             base_dict0(min_lr=8e-6, max_lr=2e-3, step_factor=5),
             base_dict0(min_lr=8e-6, max_lr=2e-3, step_factor=10),
             base_dict0(min_lr=8e-6, max_lr=2e-3, step_factor=20)
         ]
-    elif model_type == 1:
+    elif model_key == 1:
         base_dict0 = lambda min_lr, max_lr, step_factor: \
-            OrderedDict({'Model_Type': model_type, 'min_lr': min_lr, 'max_lr': max_lr, 'step_factor': step_factor})
+            OrderedDict({'Model_Type': model_key, 'min_lr': min_lr, 'max_lr': max_lr, 'step_factor': step_factor})
         dictionary = [
             base_dict0(min_lr=8e-5, max_lr=4e-2, step_factor=10),
         ]
-    model_dictionary = {model_type: dictionary}
+    model_dictionary = {model_key: dictionary}
     return model_dictionary
 
 
