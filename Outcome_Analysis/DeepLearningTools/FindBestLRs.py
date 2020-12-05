cc49586d7598a13e89ed424870ea56d9865b6996
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-05 13:15:10 -0600

    increase max lr

--- Outcome_Analysis/DeepLearningTools/FindBestLRs.py
+++ Outcome_Analysis/DeepLearningTools/FindBestLRs.py
@@ -38,8 +38,8 @@ def return_model_and_things(model_base, out_path, things):
 
 def find_best_lr(batch_size=24, model_key=0):
     base_path, morfeus_drive = return_paths()
-    min_lr = 1e-6
-    max_lr = 1
+    min_lr = 1e-5
+    max_lr = 1e2
     model_base = return_model(model_key=model_key)
     # loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
     loss = CosineLoss()
