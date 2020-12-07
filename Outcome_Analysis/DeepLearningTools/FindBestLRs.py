aad5bb0638b29f4393923b47f123763f9fd615b8
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-07 06:23:13 -0600

    just 32 filters, 32 growth_rate

--- Outcome_Analysis/DeepLearningTools/FindBestLRs.py
+++ Outcome_Analysis/DeepLearningTools/FindBestLRs.py
@@ -17,8 +17,8 @@ def return_model_and_things(model_base, out_path, things):
         for dense_conv_blocks in [1, 2, 3]:
             for dense_layers in [1, 2, 3]:
                 for num_dense_connections in [128, 256]:
-                    for filters in [16, 32]:
-                        for growth_rate in [16, 32]:
+                    for filters in [32]:
+                        for growth_rate in [32]:
                             all_list = 'blocks_in_dense_{}.dense_conv_blocks_{}.dense_layers_{}.' \
                                        'num_dense_connections{}.filters_{}.' \
                                        'growth_rate_{}'.format(blocks_in_dense, dense_conv_blocks, dense_layers,
