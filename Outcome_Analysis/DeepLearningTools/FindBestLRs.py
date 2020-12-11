24fae7d6e7615c6818b22c58e9f0eb3217a70fa0
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-11 13:07:44 -0600

    test new

--- Outcome_Analysis/DeepLearningTools/FindBestLRs.py
+++ Outcome_Analysis/DeepLearningTools/FindBestLRs.py
@@ -14,11 +14,11 @@ from tensorflow_addons.optimizers import RectifiedAdam
 
 def return_model_and_things(model_base, out_path, things):
     for blocks_in_dense in [3]:
-        for dense_conv_blocks in [1, 2, 3]:
+        for dense_conv_blocks in [2, 3]:
             for dense_layers in [2, 3]:
                 for num_dense_connections in [256]:
                     for filters in [8]:
-                        for growth_rate in [4, 8]:
+                        for growth_rate in [2, 4, 8]:
                             all_list = 'blocks_in_dense_{}.dense_conv_blocks_{}.dense_layers_{}.' \
                                        'num_dense_connections{}.filters_{}.' \
                                        'growth_rate_{}'.format(blocks_in_dense, dense_conv_blocks, dense_layers,
