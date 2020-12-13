67eafee29b4998b6420d2d29afbb165ae000e3ec
markba122@gmail.com <bmanderson@mdanderson.org>
2020-12-13 10:09:28 -0600

    try with growth of 32

--- Outcome_Analysis/DeepLearningTools/FindBestLRs.py
+++ Outcome_Analysis/DeepLearningTools/FindBestLRs.py
@@ -18,7 +18,7 @@ def return_model_and_things(model_base, out_path, things):
             for dense_layers in [0, 3, 5]:
                 for num_dense_connections in [256]:
                     for filters in [8, 16]:
-                        for growth_rate in [16]:
+                        for growth_rate in [32]:
                             all_list = 'blocks_in_dense_{}.dense_conv_blocks_{}.dense_layers_{}.' \
                                        'num_dense_connections{}.filters_{}.' \
                                        'growth_rate_{}'.format(blocks_in_dense, dense_conv_blocks, dense_layers,
