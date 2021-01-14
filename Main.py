30b66413b895cd0f252d76eba472abf9c297a6f3
markba122@gmail.com <bmanderson@mdanderson.org>
2021-01-14 10:02:52 -0600

    update to latest version of diocm reader

--- Main.py
+++ Main.py
@@ -4,8 +4,11 @@ import os
 import pandas as pd
 from scipy.ndimage.measurements import center_of_mass
 from Local_Recurrence_Work.Ray_Tracing.Utilities import *
+from Deep_Learning.Base_Deeplearning_Code.Dicom_RT_and_Images_to_Mask.src.DicomRTTool import DicomReaderWriter, plot_scroll_Image
 import time
-
+import pydicom
+import SimpleITK as sitk
+from RegisterImages.WithDicomReg import register_images_with_dicom_reg
 
 '''
 This should have two parts... first, check the recurrence image for what direction
@@ -27,45 +30,71 @@ while True:
     for index in range(len(MRNs)):
         MRN = str(data['MRN'][index])
         Recurrence = data['Recurrence'][index]
-        # if os.path.exists(os.path.join(status_path, MRN+'.txt')):
-        #     continue
+        Ablation = data['Secondary'][index]
+        if os.path.exists(os.path.join(status_path, MRN+'.txt')):
+            continue
         print(MRN)
         patient_path = os.path.join(images_path, MRN)
         if not os.path.exists(patient_path):
             print('Does not exist...')
             continue
         case = os.listdir(patient_path)[0]
+        registration_path = os.path.join(images_path, MRN, case, 'Reg')
+        reg_file = pydicom.read_file(os.path.join(registration_path, os.listdir(registration_path)[0]))
         recurrence_path = os.path.join(patient_path, case, Recurrence)
+        ablation_path = os.path.join(patient_path, case, Ablation)
         output_dir = os.path.join(recurrence_path, 'new_RT')
         RS_files = []
         if os.path.exists(output_dir):
             RS_files = [i for i in os.listdir(output_dir) if i.endswith('.dcm')]
         recurrence_reader = DicomReaderWriter(arg_max=False, Contour_Names=['Liver', 'Recurrence',
-                                                                            'Ablation_Recurrence', 'Liver_Ablation',
-                                                                            'Ablation', 'GTV_Exp_5mm_outside_Ablation'])
+                                                                            'Ablation_Recurrence',
+                                                                            'Liver_Ablation', 'Ablation',
+                                                                            'GTV_Exp_5mm_outside_Ablation'])
+        # ablation_reader = DicomReaderWriter(arg_max=False, Contour_Names=['Liver_Ablation', 'Ablation',
+        #                                                                   'GTV_Exp_5mm_outside_Ablation'])
+        '''
+        First, load up the dicom images and masks
+        '''
         try:
-            recurrence_reader.Make_Contour_From_directory(recurrence_path)
+            recurrence_reader.walk_through_folders(recurrence_path)
+            # ablation_reader.walk_through_folders(ablation_path)
         except:
             continue
-        spacing = recurrence_reader.annotation_handle.GetSpacing()
-        mask = recurrence_reader.mask
+        recurrence_reader.get_mask()
+        # ablation_reader.get_mask()
+
+        registered_mask = register_images_with_dicom_reg(fixed_image=sitk.Cast(recurrence_reader.dicom_handle,
+                                                                               sitk.sitkFloat32),
+                                                         moving_image=ablation_reader.annotation_handle,
+                                                         dicom_registration=reg_file, min_value=0,
+                                                         method=sitk.sitkNearestNeighbor)
+
+        spacing = recurrence_reader.dicom_handle.GetSpacing()
         # mask = mask[130:160,...]
+        mask = recurrence_reader.mask
         liver_recurrence = mask[..., 1]
         ablation_recurrence = mask[..., 3]
         recurrence_base = mask[..., 2]
         ablation_recurrence[liver_recurrence == 0] = 0
         recurrence_base[liver_recurrence == 0] = 0
 
-        liver_ablation = mask[..., 4]
-        ablation = mask[..., 5]
-        min_ablation_margin = mask[..., 6]
-        ablation[liver_ablation == 0] == 0
+        mask_ablation = mask[..., 3:]
+        liver_ablation = mask_ablation[..., 1]
+        ablation = mask_ablation[..., 2]
+        min_ablation_margin = mask_ablation[..., 3]
+        ablation[liver_ablation == 0] = 0
         min_ablation_margin[liver_ablation == 0] = 0
         if not RS_files:
+            '''
+            Next, create a cone that maps from the centroid of the ablation recurrence through the recurrence
+            '''
             centroid_of_ablation_recurrence = np.asarray(center_of_mass(ablation_recurrence))
             centroid_of_ablation = np.asarray(center_of_mass(ablation))
             output_recurrence = create_output_ray(centroid_of_ablation_recurrence, ref_binary_image=recurrence_base,
-                                                  spacing=spacing, min_max_only=False, target_centroid=centroid_of_ablation)
+                                                  spacing=spacing, min_max_only=False,
+                                                  target_centroid=centroid_of_ablation)
+
             recurrence_reader.with_annotations(output_recurrence, output_dir=output_dir,
                                                ROI_Names=['cone_recurrence', 'cone_projected'])
         else:
