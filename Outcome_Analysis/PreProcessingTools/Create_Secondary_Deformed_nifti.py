4e48decd12d88fa26b27cb045957a2e0e753970f
markba122@gmail.com <bmanderson@mdanderson.org>
2021-01-26 12:38:19 -0600

    any other changes

--- Outcome_Analysis/PreProcessingTools/Create_Secondary_Deformed_nifti.py
+++ Outcome_Analysis/PreProcessingTools/Create_Secondary_Deformed_nifti.py
@@ -10,20 +10,29 @@ def create_secondary_deformed_nifti(nifti_export_path, deformation_export_path, 
     patient_df = pd.read_excel(anonymized_sheet)
     deformed_image_files = [i for i in os.listdir(deformation_export_path) if i.endswith('.mhd')]
     for deformed_image in deformed_image_files:
-        MRN, primary, _, secondary = deformed_image.strip('.mhd').split('_')
-        inside_df = patient_df.loc[(patient_df['MRN'] == int(MRN)) & (patient_df['PreExam'] == primary) & (patient_df['PostExam'] == secondary)]
+        print(deformed_image)
+        split_up = deformed_image.strip('.mhd').split('_')
+        if len(split_up) == 4:
+            MRN, primary, _, secondary = split_up
+        else:
+            MRN = '{}_{}_{}'.format(split_up[0], split_up[1], split_up[2])
+            primary, secondary = split_up[3], split_up[5]
+            xxx = 1
+        inside_df = patient_df.loc[(patient_df['MRN'].astype('str') == MRN) & (patient_df['PreExam'] == primary) &
+                                   (patient_df['PostExam'] == secondary)]
         assert inside_df.shape[0] <= 1, 'Somethings wrong here..{}'.format(MRN)
         if inside_df.shape[0] == 1:
             patient_id = int(inside_df['PatientID'].values[0])
             out_path = os.path.join(nifti_export_path, '{}_Secondary_Deformed.nii'.format(patient_id))
         else:
+            print('Not found here...{}'.format(MRN))
             continue
         if os.path.exists(out_path):
             continue
         deformed_handle = sitk.ReadImage(os.path.join(deformation_export_path, deformed_image))
         primary_handle = sitk.ReadImage(os.path.join(nifti_export_path, '{}_Primary_Dicom.nii'.format(patient_id)))
         if deformed_handle.GetSize() != primary_handle.GetSize():
-            print('These are not the same for {}...'.format(MRN))
+            # print('These are not the same for {}...'.format(MRN))
             deformed_handle = sitk.Resample(deformed_handle, primary_handle, sitk.AffineTransform(3), sitk.sitkLinear,
                                             -1000, deformed_handle.GetPixelID())
         deformed_handle.SetSpacing(primary_handle.GetSpacing())
